6610e1d41c2714eb99169f17d3f316e6e3a110e2
rusinma <148705990+rusinma@users.noreply.github.com>
2023-10-22 20:58:01 +0200

    main.py

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Jenkins')
+    input1 = os.getenv("Input1")
+    print(input1)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
